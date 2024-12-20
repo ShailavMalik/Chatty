ac602dfc8fe3d85abc8974d64feb66a2b576a3f1
Shailav Malik <malikshailav@gmail.com>
2024-12-20 18:40:47 +0530

    made changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+// Import necessary modules and packages
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
@@ -9,27 +10,44 @@ import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
+import cors from "cors";
 
+// Load environment variables from .env file
 dotenv.config();
 
+// Resolve the directory name
 const __dirname = path.resolve();
-// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
+// Set the port from environment variables or default to 5000
 const PORT = process.env.PORT || 5000;
 
+// Configure CORS to allow requests from the specified origin
+app.use(
+  cors({
+    origin: "https://chatty-m3ui.onrender.com/",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+// Middleware to parse JSON payloads and cookies
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
+// Define routes for authentication, messages, and users
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+// Serve static files from the frontend build directory
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+// Handle all other routes by serving the frontend's index.html file
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
+// Start the server and connect to MongoDB
 server.listen(PORT, () => {
-	connectToMongoDB();
-	console.log(`Server Running on port ${PORT}`);
+  connectToMongoDB();
+  console.log(`Server Running on port ${PORT}`);
 });
